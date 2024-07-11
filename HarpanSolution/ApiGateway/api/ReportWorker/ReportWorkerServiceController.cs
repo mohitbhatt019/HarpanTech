f8a2c1b623bf2602909ec31f17fdafe1a0a810bd
CSPC449\DOTNET <mohit-bhatt@cssoftsolutions.com>
2024-07-11 18:33:14 +0530

    Created ApiGateway

---
+++ HarpanSolution/ApiGateway/api/ReportWorker/ReportWorkerServiceController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiGateway.api.ReportWorker
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ReportWorkerServiceController : ControllerBase
+    {
+    }
+}
